c193fe5c0079b7911008ba6a3646f74f6d38771e
rjasonroberts <rjasonroberts@gmail.com>
2015-04-14 18:38:30 -0600

    Fixed enter key navigating to home page when input field had focus.

--- start.js
+++ start.js
@@ -1,14 +1,13 @@
 var User = Backbone.Model.extend({
-  defaults : {
-  	id: 1,
-    name: "Jason",
-    score: 0
-  },
- 
-  initialize : function() {
-    this.user = new User;
-    this.user.url = 'user/' + this.id;
-  }
+ defaults : {
+ 	id: 1,
+   name: "Jason",
+   score: 0
+ },
+ initialize : function() {
+   this.user = new User;
+   this.user.url = 'user/' + this.id;
+ }
 });
 
 var AppView = Backbone.View.extend({
@@ -18,7 +17,7 @@ var AppView = Backbone.View.extend({
 	initialRender: function(){
 		this.container = $('<div>')
 			.attr('id', 'container')
-			.appendTo(this.el)
+			.appendTo(this.el);
 
 		this.header = $('<div>')
 			.addClass('header')
@@ -26,7 +25,7 @@ var AppView = Backbone.View.extend({
 		
 		this.headerTitle = $('<h1>')			
 			.text('Let\'s Do Some Math')
-			.appendTo(this.header)
+			.appendTo(this.header);
 
 		this.mainContent = $('<div>')
 			.addClass('mainContent')
@@ -35,7 +34,7 @@ var AppView = Backbone.View.extend({
 		this.startHeader = $('<h3>')
 			.addClass('startHeader')
 			.text('Hit the start button and let\'s get started.')
-			.appendTo(this.mainContent)	
+			.appendTo(this.mainContent);
 
 		this.startButton = $('<button>')
 			.addClass('startBtn btn btn-success')
@@ -43,7 +42,9 @@ var AppView = Backbone.View.extend({
 			.attr('value', 'Start')
 			.text('Start')
 			.appendTo(this.mainContent)
-			.bind('click', this.startGame)
+			.bind('click', this.startGame);
+
+		this;
 	},
 	render: function(){
 		return;
@@ -55,9 +56,6 @@ var AppView = Backbone.View.extend({
 			el: $('#all')
 		});
 		return
-	},
-	score: function(){
-		return
 	}
 });
 
@@ -67,14 +65,13 @@ QuestionsModel = Backbone.Model.extend({});
 
 var QuestionsView = Backbone.View.extend({
 	events: {
-        'enter': 'answer1'
+        'keyup #answer': 'checkAnswer'
     },
 	initialize: function(){
 		var number1;
 		var number2;
 		var userAnswer;
 		var actualAnswer;
-
 		var problemDiv = $("#problem");
 		var statusDiv = $("#status");
 		var answer = $("#answer");
@@ -84,37 +81,34 @@ var QuestionsView = Backbone.View.extend({
 	initialRender: function(){	
 		this.questionContainer = $('<div>')
 			.addClass('questionContainer')
-			.appendTo(this.el)
-
-		this.formContainer = $('<form>')
-			.appendTo(this.questionContainer)
+			.appendTo(this.el);
 
 		this.problem = $('<h3>')
 			.attr('id', 'problem')
-			.appendTo(this.formContainer)
+			.appendTo(this.questionContainer);
 
 		this.divForAnswerandBtn = $('<div>')
 			.addClass('input-append')
-			.appendTo(this.formContainer)		
+			.appendTo(this.questionContainer);
 
 		this.answer = $('<input>')
 			.attr('type', 'text')
 			.attr('id', 'answer')
 			.attr('placeholder', 'answer...')
 			.addClass('form-control span2')			
-			.appendTo(this.divForAnswerandBtn)
+			.appendTo(this.divForAnswerandBtn);		
 
 		this.solve = $('<input>')
 			.attr('value','Solve')
 			.addClass('solve btn')
 			.attr('type', 'button')
 			.appendTo(this.divForAnswerandBtn)
-			.bind('click', this.answer1)
+			.bind('click', this.checkAnswer);
 
 		this.statusDiv = $('<div>')
 			.attr('id', 'status')
 			.text('Click the Solve button to Solve the problem')
-			.appendTo(this.formContainer)
+			.appendTo(this.questionContainer);
 
 		this.nextProblem = $('<button>')
 			.addClass('nextProblem btn btn-success')
@@ -123,13 +117,20 @@ var QuestionsView = Backbone.View.extend({
 			.css('display', 'none')
 			.text('Next')
 			.appendTo(this.formContainer)
-			.bind('click', this.nextProblem)
+			.bind('click', this.nextProblem);
+
+		this.score= $('<div>')
+			.addClass('score')
+			.text('0')
+			.appendTo(this.questionContainer);
 
 		number1 = Math.floor(1 + Math.random() * 9);
 		number2 = Math.floor(1 + Math.random() * 9);
 		this.problem.text("How much is " + number1 + " x " + number2 + " ?");
 		actualAnswer = (number1 * number2);
+		var score = 0;
 
+		this;
 	},
 	nextProblem: function(){
 		$('.questionContainer').remove();
@@ -139,24 +140,36 @@ var QuestionsView = Backbone.View.extend({
 		});
 		return
 	},
-	answer1: function(){		
-		userAnswer = answer.value;
-		answer.value = "";
-		if (parseInt(userAnswer,10) === actualAnswer){
-			$('#answer').remove();
-			$('.solve').remove();
-			this.correctAnswer = $('<div>')
-				.attr('id', 'correctAnswer')
-				.text(actualAnswer)
-				.appendTo($('#problem'))
+	checkAnswer: function(e){
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if(keycode == 13){
+			userAnswer = answer.value;
+			answer.value = "";
+			if (parseInt(userAnswer,10) === actualAnswer){
+				$('#answer').remove();
+				$('.solve').remove();
 
-			$("#status").text("Very good!");
-			$('.nextProblem').css('display', 'inline');
+				this.correctAnswer = $('<div>')
+					.attr('id', 'correctAnswer')
+					.text(actualAnswer)
+					.appendTo($('#problem'))
 
+				$("#status").text('Correct!');
+				this.score();
+				//Is this working????
+				this.nextProblem.css('display');
+
+				$('.nextProblem').css('display', 'inline');
+			}
+			else {
+				$("#status").text("No. Please try again");
+			}
 		}
-		else {
-			$("#status").text("No. Please try again");
-		}
+		return
+	},
+	score: function(){
+		score++
+		this.score.text(score);
 		return
 	}
 
